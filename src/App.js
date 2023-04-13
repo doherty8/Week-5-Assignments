4bb134376f7dbe24f5b3d77c775ebf9d2a2c8313
Craig Doherty <craig@Craigs-MacBook-Pro.local>
2023-04-13 09:46:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
-function App() {
+const Calculator = () => {
+  const [expression, setExpression] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleDigitClick = (digit) => {
+    setExpression((prevExpression) => prevExpression + digit);
+  };
+
+  const handleOperationClick = (operator) => {
+    const lastChar = expression.slice(-1);
+    if (lastChar === "+" || lastChar === "-" || lastChar === "*" || lastChar === "/") {
+      setExpression((prevExpression) => prevExpression.slice(0, -1) + operator);
+    } else {
+      setExpression((prevExpression) => prevExpression + operator);
+    }
+  };
+  
+
+  const handleEqualsClick = () => {
+    try {
+      const answer = eval(expression);
+      setResult(answer);
+    } catch (error) {
+      setResult("Invalid expression");
+    }
+  };
+
+  const handleClearClick = () => {
+    setExpression("");
+    setResult("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <div className="calcContainer">
+      <div className="display">{result || expression || "0"}</div>
+      <div className="buttons">
+        <div className="buttons1">
+        <button onClick={() => handleDigitClick("7")}>7</button>
+        <button onClick={() => handleDigitClick("8")}>8</button>
+        <button onClick={() => handleDigitClick("9")}>9</button>
+        <button onClick={() => handleOperationClick("+")}>+</button>
+        </div>
+        <div className="buttons2">
+        <button onClick={() => handleDigitClick("4")}>4</button>
+        <button onClick={() => handleDigitClick("5")}>5</button>
+        <button onClick={() => handleDigitClick("6")}>6</button>
+        <button onClick={() => handleOperationClick("-")}>-</button>
+        </div>
+        <div className="buttons3">
+        <button onClick={() => handleDigitClick("1")}>1</button>
+        <button onClick={() => handleDigitClick("2")}>2</button>
+        <button onClick={() => handleDigitClick("3")}>3</button>
+        <button onClick={() => handleOperationClick("*")}>*</button>
+        </div>
+        <div className="buttons4">
+        <button onClick={() => handleDigitClick("0")}>0</button>
+        <button onClick={() => handleOperationClick("/")}>/</button>
+        <button onClick={handleClearClick}>C</button>
+        <button onClick={handleEqualsClick}>=</button>
+        </div>
+      </div>
+    </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Calculator;
